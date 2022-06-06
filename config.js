d0dfb530c5b0334debf714c95f108be3622bd145
unknown <anocch27@gmail.com>
2022-06-06 17:36:21 +0700

    12344

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Hee loo ! 🖐🖐🖐',
+    introDesc: `tớ có điều này muốn hỏi cậu, cậu nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'OK',
+    title: 'cậu thích tớ có phải không ? 😙',
+    desc: 'cậu không trả lời hoặc thoát ra, tức là cậu thích tớ đó nha :> ',
+    btnYes: 'Có, tớ thật sự thích cậu 💖💖💖',
+    btnNo: '💔 bạn mơ à ! 💔 ',
+    question:'Nói cho tớ biết đi vì sao cậu thích tớ 😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Tại vì anh đẹp trai đó Ahiiiii 😊😊',
+    mess: 'Anh biết mà, Anh cũng thích em lắm 🥰. Love you 300.000 😘😘',
+    messDesc: '💬💬 Nhắn tin cho Anh nha. 💬💬',
+    btnAccept: 'Ok lun 💖',
+    messLink: 'https://www.facebook.com/luongan0402/'
+}
